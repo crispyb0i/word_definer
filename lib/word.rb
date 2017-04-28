69225bd987afb27b67707bb8c76ce8f367681494
David Shin <xdavidshinx@gmail.com>
2017-04-28 10:52:41 -0700

    add base files and update spec and word/definition.rb files

---
+++ lib/word.rb
@@ -0,0 +1,37 @@
+class Word
+  @@words = []
+
+  attr_accessor(:word_content, :definitions, :id)
+
+  define_method(:initialize) do |attributes|
+    @word_content = attributes.fetch(:word_content)
+    @definitions = []
+    @id = @@words.length().+(1)
+  end
+
+  define_method(:add_definition) do |definition|
+    @definitions.push(definition)
+  end
+
+  define_method(:save) do
+    @@words.push(self)
+  end
+
+  define_singleton_method(:all) do
+    @@words
+  end
+
+  define_singleton_method(:clear) do
+    @@words = []
+  end
+
+  define_singleton_method(:find) do |identification|
+    found_word = nil
+    @@words.each() do |word|
+      if word.id.eql?(identification)
+        found_word = word
+      end
+    end
+    found_word
+  end
+end
---
+++ spec/word_definer_spec.rb
@@ -0,0 +1,31 @@
+require('rspec')
+require('word')
+require('definition')
+
+describe(Definition) do
+  before() do
+    Definition.clear()
+  end
+
+  describe("#save") do
+    it("pushes a definition to an array that holds all the definitions") do
+      test_definition = Definition.new({:definition_of_word => "A person who is really strong"})
+      test_definition.save()
+      expect(Definition.all()).to(eq([test_definition]))
+    end
+  end
+
+  describe(".all") do
+    it("is empty at first") do
+      expect(Definition.all()).to(eq([]))
+    end
+  end
+
+  describe(".clear") do
+    it("empties the array that holds all the definitions") do
+      test_definition = Definition.new({:definition_of_word => "A person who is really strong."}).save()
+      Definition.clear()
+      expect(Definition.all()).to(eq([]))
+    end
+  end
+end
